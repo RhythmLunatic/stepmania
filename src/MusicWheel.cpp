54820936731435468f61a0865666450c21ca12dc
RhythmLunatic <kirbyrulez@att.net>
2019-04-16 22:11:39 -0500

    Make musicwheel sections gettable through lua

--- src/MusicWheel.cpp
+++ src/MusicWheel.cpp
@@ -1470,6 +1470,20 @@ void MusicWheel::SetOpenSection( RString group )
 	RebuildWheelItems();
 }
 
+//TODO: Maybe optimize and use an array instead. Need to check later.
+void MusicWheel::GetCurrentSections(vector<RString> &sections)
+{
+	vector<MusicWheelItemData *> &wiWheelItems = getWheelItemsData(GAMESTATE->m_SortOrder);
+	//vector<RString> sections;
+	for( unsigned i = 0; i < wiWheelItems.size(); i++ )
+	{
+		//MusicWheelItemData &d = *from[i];
+		if ( wiWheelItems[i]->m_Type == WheelItemDataType_Section && !wiWheelItems[i]->m_sText.empty())
+			sections.push_back(wiWheelItems[i]->m_sText);
+	}
+	//return sections;
+}
+
 // sm-ssc additions: jump to group
 RString MusicWheel::JumpToNextGroup()
 {
@@ -1700,6 +1714,19 @@ public:
 		return 1;
 	}
 	DEFINE_METHOD(GetSelectedSection, GetSelectedSection());
+	static int GetCurrentSections( T* p, lua_State *L )
+	{
+		vector<RString> v;
+		p->GetCurrentSections(v);
+		
+		//vector<RString> v;
+		
+		//v.push_back("asdadasdads");
+		
+		LuaHelpers::CreateTableFromArray<RString>( v, L );
+		return 1;
+	
+	}
 	static int IsRouletting( T* p, lua_State *L ){ lua_pushboolean( L, p->IsRouletting() ); return 1; }
 	static int SelectSong( T* p, lua_State *L )
 	{
@@ -1740,6 +1767,7 @@ public:
 		ADD_METHOD( SelectSong );
 		ADD_METHOD( SelectCourse );
 		ADD_METHOD( Move );
+		ADD_METHOD( GetCurrentSections );
 	}
 };
 
