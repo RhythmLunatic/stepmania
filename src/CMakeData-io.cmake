38ff072a086bd3c10f7ba3e3b41bb830bf71201a
RhythmLunatic <kirbyrulez@att.net>
2019-05-10 19:27:26 -0500

    PIUIO?

--- src/CMakeData-io.cmake
+++ src/CMakeData-io.cmake
@@ -2,12 +2,14 @@ list(APPEND SMDATA_IO_SRC
   "io/Python23IO.cpp"
   "io/USBDevice_Libusb.cpp"
   "io/USBDriver.cpp"
+  "io/PIUIO.cpp"
 )
 
 list(APPEND SMDATA_IO_HPP
   "io/Python23IO.h"
   "io/USBDevice.h"
   "io/USBDriver.h"
+  "io/PIUIO.h"
 )
 
 
