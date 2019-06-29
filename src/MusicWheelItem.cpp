4460984ce5818944f50c7bb3b8bf95f56eb4c539
RhythmLunatic <kirbyrulez@att.net>
2019-06-28 23:34:22 -0500

    remove duplicate SetMessage

--- src/MusicWheelItem.cpp
+++ src/MusicWheelItem.cpp
@@ -383,54 +383,7 @@ void MusicWheelItem::HandleMessage( const Message &msg )
 	    msg == Message_CurrentTrailP2Changed ||
 	    msg == Message_PreferredDifficultyP1Changed ||
 	    msg == Message_PreferredDifficultyP2Changed )
-	{
-		const MusicWheelItemData *pWID = dynamic_cast<const MusicWheelItemData*>( m_pData );
-		MusicWheelItemType type = MusicWheelItemType_Invalid;
-
-		switch( pWID->m_Type )
-		{
-			DEFAULT_FAIL( pWID->m_Type );
-			case WheelItemDataType_Song:
-				type = MusicWheelItemType_Song;
-				break;
-			case WheelItemDataType_Section:
-				if( GAMESTATE->sExpandedSectionName == pWID->m_sText )
-					type = MusicWheelItemType_SectionExpanded;
-				else
-					type = MusicWheelItemType_SectionCollapsed;
-				break;
-			case WheelItemDataType_Course:
-				type = MusicWheelItemType_Course;
-				break;
-			case WheelItemDataType_Sort:
-				if( pWID->m_pAction->m_pm != PlayMode_Invalid )
-					type = MusicWheelItemType_Mode;
-				else
-					type = MusicWheelItemType_Sort;
-				break;
-			case WheelItemDataType_Roulette:
-				type = MusicWheelItemType_Roulette;
-				break;
-			case WheelItemDataType_Random:
-				type = MusicWheelItemType_Random;
-				break;
-			case WheelItemDataType_Portal:
-				type = MusicWheelItemType_Portal;
-				break;
-			case WheelItemDataType_Custom:
-				type = MusicWheelItemType_Custom;
-				break;
-		}
-
-		Message msg( "Set" );
-		msg.SetParam( "Song", pWID->m_pSong );
-		msg.SetParam( "Course", pWID->m_pCourse );
-		msg.SetParam( "Text", pWID->m_sText );
-		msg.SetParam( "Type", MusicWheelItemTypeToString(type) );
-		msg.SetParam( "Color", pWID->m_color );
-		msg.SetParam( "Label", pWID->m_sLabel );
-		this->HandleMessage( msg );
-		
+    {
 		RefreshGrades();
 	}
 
